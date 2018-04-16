486cf3118a21340d71c88cd2defaf03bc4dd9b5f
wantora <wantora@gmail.com>
2018-04-17 01:55:53 +0900

    fix

--- src/flux.js
+++ src/flux.js
@@ -1,7 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const FluxContext = React.createContext("flux");
+const FluxContext = React.createContext(null);
 
 class Dispatcher {
   constructor() {
